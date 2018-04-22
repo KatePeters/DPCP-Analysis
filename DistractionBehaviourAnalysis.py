3b3f65f89f08012f3ddeaa01857965fb536bac69
KP <kp259@le.ac.uk>
2018-04-22 08:18:45 +0100

    Reading in data
    Setting filepaths and reading in data from metafiles, storing info, writing MedFile reader loop to go through all male and female data

--- DistractionBehaviourAnalysis.py
+++ DistractionBehaviourAnalysis.py
@@ -6,21 +6,30 @@ Created on Mon Apr 16 11:18:00 2018
 @author: u1490431
 """
 
-#(1) Read in each rats licks only (from med) for each day 
+#(1) Find and extract info from the meatfile(s) 
+! Fix or delete missing files problem 
+¡ Save metafiles as .csvs not xlxs (move them to the assigned folder)
 
-DO THIS ALL TWICE. ONCE FOR MALES AND ONCE FOR FEMALES
-Store them as lickDataDPCPFemales
-Store as lickDataDPCPMales
+# insert PC filepath here also for easy switching between PC and MAC 
 
-#(1)
+matafile_males = '/Volumes/KP_HARD_DRI/kp259/DPCP_ALL/DPCP1&2Masterfile.csv'
+metafile_females = '/Volumes/KP_HARD_DRI/kp259/DPCP_ALL/DPCP3_Metafile.csv'
 
-find the meatfile
+extract_males = MetaExtractor(metafile_males)
+extract_females = MetaExtractor(metafile_females)
 
-go to metafile and produce metaextractor
+# Useful info for sanity checks, and stored list of file names easy to access
+folder = '/Volumes/KP_HARD_DRI/kp259/DPCP_ALL/'
+# Folder with ALL files inside it - add them all to this folder 
 
 
-#(2)
+#(2) - get all lick data for all rats, variables extracted from metafile 
+#    - need licks onset and licks offset values from the med files for all
+MALES
+
 for each filename in the names (produced from metafilereader output):
+    if include = 1 then index the same bit and run
+    filename = folder + filename from loop 
     only those that are included in col (1)
     open the file, medfilereader it 
     produce the lick variable 
@@ -28,12 +37,22 @@ for each filename in the names (produced from metafilereader output):
     = lickday data for all rats, do this twice. Once for the males (combined)
     and once for the females (total)
 
+FEMALES
+
+
+! With the lickcalc output, add it to a master list of lists or dictionary/list 
+of dictionaries so that it can be indexed by file / rat / day 
+
+A male dictionary and a female discitonary / list of lists is probably best 
+
+
 #(3)    
 for distraction days for all rats (all after the last lick, for cols with 2):
     take that lick variable and run DistractioCalc2 (on just distraction days, and modelled?)
     take d produced from DistractionCalc2 and run distractedOrNot on all
     store in a list called --> distracted or not 
 
+! Sanity check that numbers are what expected (with small differences for med issue)
 #(4) Sort by day, treatment and sex
 # Analyse licking only 
 # Metafile extractor will produce the lists of which rats are which (twice m and f)
@@ -116,3 +135,11 @@ ANOVA - effects of sex on distraction OR 3 way ANOVA (although 2 cohorts far app
 
 # Correlations 
 
+# Individual variations in burst number, cluster number and mean INTERBURST INTERVAL
+    # compared (correlation or regression) to PDP and distracted percentage 
+    
+    # Saline 
+    
+    
+    
+    

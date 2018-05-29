4d8091f3d0e140f0867fa412ba168c57ff8ee025
KP <kp259@le.ac.uk>
2018-05-29 11:12:31 +0100

    percent distracted by group func

---
+++ percentdis.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 29 11:11:03 2018
+
+@author: u1490431
+"""
+
+Percentage distracted by group / day 
+
+def percentdisgroup():
+    return
+
+
+
